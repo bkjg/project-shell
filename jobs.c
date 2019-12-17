308f010ac5d9504fba3509390ab5a7606347e68b
Barbara Kaczorowska <bkjg252@gmail.com>
2019-12-17 18:18:21 +0100

    Initial commit

---
+++ jobs.c
@@ -0,0 +1,338 @@
+#include "shell.h"
+
+typedef struct proc {
+  pid_t pid;    /* process identifier */
+  int state;    /* RUNNING or STOPPED or FINISHED */
+  int exitcode; /* -1 if exit status not yet received */
+} proc_t;
+
+typedef struct job {
+  pid_t pgid;    /* 0 if slot is free */
+  proc_t *proc;  /* array of processes running in as a job */
+  int nproc;     /* number of processes */
+  int state;     /* changes when live processes have same state */
+  char *command; /* textual representation of command line */
+} job_t;
+
+static job_t *jobs = NULL; /* array of all jobs */
+static int njobmax = 1;    /* number of slots in jobs array */
+static int tty_fd = -1;    /* controlling terminal file descriptor */
+
+static proc_t* findPid(job_t job, pid_t pid) {
+  for (int i = 0; i < job.nproc; ++i) {
+    if (job.proc[i].pid == pid) {
+      return &job.proc[i];
+    }
+  }
+
+  return NULL;
+}
+
+static int jobStatusChanged(job_t job) {
+  for (int i = 1; i < job.nproc; ++i) {
+    if (job.proc[i].state != job.proc[i - 1].state) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+static void sigchld_handler(int sig) {
+  int old_errno = errno;
+  pid_t pid;
+  int status;
+//#ifdef DEBUG
+  printf("JOBS: sigchld_handler - TODO (implemented)\n");
+//#endif
+  /* TODO: Change state (FINISHED, RUNNING, STOPPED) of processes and jobs.
+   * Bury all children that finished saving their status in jobs. */
+  while ((pid = waitpid(-1, &status, WNOHANG | WUNTRACED | WCONTINUED)) > 0) {
+    for (int i = 0; i < njobmax; ++i) {
+      if (jobs[i].pgid != 0) {
+        proc_t *proc = findPid(jobs[i], pid);
+        if (proc == NULL) continue;
+        if (WIFEXITED(status) || WIFSIGNALED(status)) {
+          proc->state = FINISHED;
+          proc->exitcode = status;
+        } else if (WIFSTOPPED(status)) {
+          proc->state = STOPPED;
+        } else if (WIFCONTINUED(status)) {
+          proc->state = RUNNING;
+        }
+
+        if (jobStatusChanged(jobs[i])) {
+          jobs[i].state = jobs[i].proc[0].state;
+        }
+
+        break;
+      }
+    }
+  }
+
+  errno = old_errno;
+}
+
+/* When pipeline is done, its exitcode is fetched from the last process. */
+static int exitcode(job_t *job) {
+#ifdef DEBUG
+  printf("JOBS: exitcode (implemented)\n");
+  printf("exitcode: %d\n", job->nproc);
+#endif
+  return job->proc[job->nproc - 1].exitcode;
+}
+
+static int allocjob(void) {
+#ifdef DEBUG
+  printf("JOBS: allocjob (implemented)\n");
+#endif
+  /* Find empty slot for background job. */
+  for (int j = BG; j < njobmax; j++)
+    if (jobs[j].pgid == 0)
+      return j;
+
+  /* If none found, allocate new one. */
+  jobs = realloc(jobs, sizeof(job_t) * (njobmax + 1));
+  return njobmax++;
+}
+
+static int allocproc(int j) {
+#ifdef DEBUG
+  printf("JOBS: allocproc (implemented)\n");
+#endif
+  job_t *job = &jobs[j];
+  job->proc = realloc(job->proc, sizeof(proc_t) * (job->nproc + 1));
+  return job->nproc++;
+}
+
+int addjob(pid_t pgid, int bg) {
+#ifdef DEBUG
+  printf("JOBS: addjob (implemented)\n");
+#endif
+  int j = bg ? allocjob() : FG;
+  job_t *job = &jobs[j];
+  /* Initial state of a job. */
+  job->pgid = pgid;
+  job->state = RUNNING;
+  job->command = NULL;
+  job->proc = NULL;
+  job->nproc = 0;
+  return j;
+}
+
+static void deljob(job_t *job) {
+#ifdef DEBUG
+  printf("JOBS: deljob (implemented)\n");
+#endif
+  assert(job->state == FINISHED);
+  free(job->command);
+  free(job->proc);
+  job->pgid = 0;
+  job->command = NULL;
+  job->proc = NULL;
+  job->nproc = 0;
+}
+
+static void movejob(int from, int to) {
+#ifdef DEBUG
+  printf("JOBS: movejob (implemented)\n");
+#endif
+  assert(jobs[to].pgid == 0);
+  memcpy(&jobs[to], &jobs[from], sizeof(job_t));
+  memset(&jobs[from], 0, sizeof(job_t));
+}
+
+static void mkcommand(char **cmdp, char **argv) {
+#ifdef DEBUG
+  printf("JOBS: mkcommand (implemented)\n");
+#endif
+  if (*cmdp)
+    strapp(cmdp, " | ");
+
+  for (strapp(cmdp, *argv++); *argv; argv++) {
+    strapp(cmdp, " ");
+    strapp(cmdp, *argv);
+  }
+}
+
+void addproc(int j, pid_t pid, char **argv) {
+  assert(j < njobmax);
+  job_t *job = &jobs[j];
+#ifdef DEBUG
+  printf("JOBS: addproc (implemented)\n");
+  printf("addproc - j: %d\n", j);
+#endif
+
+  int p = allocproc(j);
+  proc_t *proc = &job->proc[p];
+  /* Initial state of a process. */
+  proc->pid = pid;
+  proc->state = RUNNING;
+  proc->exitcode = -1;
+  mkcommand(&job->command, argv);
+}
+
+/* Returns job's state.
+ * If it's finished, delete it and return exitcode through statusp. */
+int jobstate(int j, int *statusp) {
+  assert(j < njobmax);
+  job_t *job = &jobs[j];
+  int state = job->state;
+#ifdef DEBUG
+  printf("JOBS: jobstate - TODO (implemented)\n");
+#endif
+
+  /* TODO: Handle case where job has finished. */
+  if (state == FINISHED && job->pgid != 0) {
+    *statusp = exitcode(job);
+    deljob(job);
+  }
+
+  return state;
+}
+
+char *jobcmd(int j) {
+  assert(j < njobmax);
+#ifdef DEBUG
+  printf("JOBS: jobcmd\n");
+#endif
+  job_t *job = &jobs[j];
+  return job->command;
+}
+
+/* Continues a job that has been stopped. If move to foreground was requested,
+ * then move the job to foreground and start monitoring it. */
+bool resumejob(int j, int bg, sigset_t *mask) {
+#ifdef DEBUG
+  printf("JOBS: resumejob - TODO (implemented)\n");
+#endif
+  if (j < 0) {
+    for (j = njobmax - 1; j > 0 && jobs[j].state == FINISHED; j--)
+      continue;
+  }
+
+  if (j >= njobmax || jobs[j].state == FINISHED)
+    return false;
+
+  /* TODO: Continue stopped job. Possibly move job to foreground slot. */
+  assert (j != FG);
+
+  Signal(SIGCONT, sigchld_handler);
+
+  kill(-jobs[j].pgid, SIGCONT);
+  jobs[j].state = RUNNING;
+
+  /* foreground job */
+  if (!bg) {
+    movejob(j, FG);
+    (void) monitorjob(mask);
+  }
+
+  return true;
+}
+
+/* Kill the job by sending it a SIGTERM. */
+bool killjob(int j) {
+#ifdef DEBUG
+  printf("JOBS: killjob - TODO (implemented)\n");
+#endif
+  if (j >= njobmax || jobs[j].state == FINISHED)
+    return false;
+  debug("[%d] killing '%s'\n", j, jobs[j].command);
+
+  /* TODO: I love the smell of napalm in the morning. */
+  int k = kill(-jobs[j].pgid, SIGTERM);
+  assert(k == 0);
+
+  return true;
+}
+
+/* Report state of requested background jobs. Clean up finished jobs. */
+void watchjobs(int which) {
+#ifdef DEBUG
+  printf("JOBS: watchjobs - TODO (implemented)\n");
+  printf("watchjobs %d %d\n", BG, njobmax);
+#endif
+  for (int j = BG; j < njobmax; j++) {
+    if (jobs[j].pgid == 0)
+      continue;
+
+    /* TODO: Report job number, state, command and exit code or signal. */
+    int statusp;
+
+#ifdef DEBUG
+    printf("jobstate - which -> %d - j: %d - status: %d", which, j, jobs[j].state);
+#endif
+    if (jobstate(j, &statusp) == which) {
+      msg("[%d] ", j);
+      if (WIFEXITED(statusp)) {
+        msg("exited, status=%d, command=%s\n", WEXITSTATUS(statusp), jobcmd(j));
+      } else if (WIFSIGNALED(statusp)) {
+        msg("killed by signal %d, command=%s\n", WTERMSIG(statusp), jobcmd(j));
+      } else if (WIFSTOPPED(statusp)) {
+        msg("stopped by signal %d, command=%s\n", WSTOPSIG(statusp), jobcmd(j));
+      } else if (WIFCONTINUED(statusp)) {
+        msg("continued, command=%s\n", jobcmd(j));
+      }
+    }
+  }
+}
+
+/* Monitor job execution. If it gets stopped move it to background.
+ * When a job has finished or has been stopped move shell to foreground. */
+int monitorjob(sigset_t *mask) {
+  int exitcode, state;
+#ifdef DEBUG
+  printf("SHELL: monitorjob - TODO\n");
+#endif
+  /* TODO: Following code requires use of Tcsetpgrp of tty_fd. */
+  /*Signal(SIGTSTP, sigchld_handler);
+  while (true) {
+    //(void) jobstate(j, &exitcode);
+    printf("State %d\n", exitcode);
+    if (exitcode < 0) {
+      Sigsuspend(mask);
+    } else {
+      break;
+    }
+  }*/
+
+  return exitcode;
+}
+
+/* Called just at the beginning of shell's life. */
+void initjobs(void) {
+  Signal(SIGCHLD, sigchld_handler);
+  jobs = calloc(sizeof(job_t), 1);
+#ifdef DEBUG
+  printf("JOBS: initjobs (implemented)\n");
+#endif
+  /* Assume we're running in interactive mode, so move us to foreground.
+   * Duplicate terminal fd, but do not leak it to subprocesses that execve. */
+  assert(isatty(STDIN_FILENO));
+  tty_fd = Dup(STDIN_FILENO);
+  fcntl(tty_fd, F_SETFL, O_CLOEXEC);
+  Tcsetpgrp(tty_fd, getpgrp());
+}
+
+/* Called just before the shell finishes. */
+void shutdownjobs(void) {
+  sigset_t mask;
+  Sigprocmask(SIG_BLOCK, &sigchld_mask, &mask);
+#ifdef DEBUG
+  printf("JOBS: shutdownjobs - TODO (implemented?)\n");
+#endif
+  /* TODO: Kill remaining jobs and wait for them to finish. */
+  for (int j = BG; j < njobmax; ++j) {
+    if (jobs[j].state != FINISHED) {
+      killjob(j);
+      Waitpid(-jobs[j].pgid, NULL, 0);
+    }
+  }
+
+  watchjobs(FINISHED);
+
+  Sigprocmask(SIG_SETMASK, &mask, NULL);
+
+  Close(tty_fd);
+}
---
+++ shell.c
@@ -0,0 +1,239 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+
+//#define DEBUG 0
+#define BACKGROUND 1
+#include "shell.h"
+
+sigset_t sigchld_mask;
+
+static sigjmp_buf loop_env;
+
+static void sigint_handler(int sig) {
+  siglongjmp(loop_env, sig);
+}
+
+/* Consume all tokens related to redirection operators.
+ * Put opened file descriptors into inputp & output respectively. */
+static int do_redir(token_t *token, int ntokens, int *inputp, int *outputp) {
+  token_t mode = NULL; /* T_INPUT, T_OUTPUT or NULL */
+  int n = 0;           /* number of tokens after redirections are removed */
+#ifdef DEBUG
+  safe_printf("SHELL: do_redir - TODO\n");
+#endif
+  for (int i = 0; i < ntokens; i++) {
+    /* TODO: Handle tokens and open files as requested. */
+    if (separator_p(token[i])) {
+      if (token[i] == T_AND) {
+
+      } else if (token[i] == T_OR) {
+
+      } else if (token[i] == T_PIPE) {
+
+      } else if (token[i] == T_BGJOB) {
+
+      } else if (token[i] == T_COLON) {
+
+      }
+      token[n] = token[i];
+      n++;
+    } else if (string_p(token[i])) {
+      token[n] = token[i];
+      n++;
+    } else {
+      assert(i + 1 < ntokens);
+      if (token[i] == T_INPUT) {
+        *inputp = open(token[i + 1], O_RDONLY, 0);
+        assert(*inputp >= 0); 
+      } else if (token[i] == T_OUTPUT) {
+        *outputp = open(token[i + 1], O_CREAT | O_WRONLY, 0666);
+        assert(*outputp >= 0); 
+      } else if (token[i] == T_APPEND) {
+        *outputp = open(token[i + 1], O_CREAT | O_WRONLY | O_APPEND, 0666);
+        assert(*outputp >= 0); 
+      } else {
+        /* BANG operator */
+      }
+    }
+  }
+
+  for (int i = n; i < ntokens; ++i) {
+    token[i] = T_NULL;
+  }
+
+  token[n] = NULL;
+
+  return n;
+}
+
+/* Execute internal command within shell's process or execute external command
+ * in a subprocess. External command can be run in the background. */
+static int do_job(token_t *token, int ntokens, bool bg) {
+  int input = -1, output = -1;
+  int exitcode = 0;
+
+#ifdef DEBUG
+  safe_printf("SHELL: do_job\n");
+#endif
+
+  ntokens = do_redir(token, ntokens, &input, &output);
+
+  if ((exitcode = builtin_command(token)) >= 0)
+    return exitcode;
+
+  sigset_t mask;
+  Sigprocmask(SIG_BLOCK, &sigchld_mask, &mask);
+
+  /* TODO: Start a subprocess, create a job and monitor it. */
+#ifdef DEBUG
+  safe_printf("SHELL: do_job - TODO\n");
+#endif
+  pid_t pid = Fork();
+
+  if (pid == 0) {  
+    Sigprocmask(SIG_SETMASK, &mask, NULL);
+    Setpgid(getpid(), getpid());
+    external_command(token);
+  }
+
+  int j = addjob(getpgid(pid), bg);
+  addproc(j, pid, token);
+
+  /* if (!bg)
+  exitcode = monitorjob(&mask);
+  printf("do_job: bg = %d\n", bg); */
+  if (!bg) {
+    while (true) {
+      monitorjob(&mask);
+      (void) jobstate(j, &exitcode);
+      printf("State %d\n", exitcode);
+      if (exitcode < 0) {
+        Sigsuspend(&mask);
+      } else {
+        break;
+      }
+    }
+  }
+
+  Sigprocmask(SIG_SETMASK, &mask, NULL);
+  return exitcode;
+}
+
+/* Start internal or external command in a subprocess that belongs to pipeline.
+ * All subprocesses in pipeline must belong to the same process group. */
+static pid_t do_stage(pid_t pgid, sigset_t *mask, int input, int output,
+                      token_t *token, int ntokens) {
+  ntokens = do_redir(token, ntokens, &input, &output);
+#ifdef DEBUG
+  printf("SHELL: do_stage\n");
+#endif
+  /* TODO: Start a subprocess and make sure it's moved to a process group. */
+  pid_t pid;
+
+  return pid;
+}
+
+static void mkpipe(int *readp, int *writep) {
+  int fds[2];
+  Pipe(fds);
+  *readp = fds[0];
+  *writep = fds[1];
+}
+
+/* Pipeline execution creates a multiprocess job. Both internal and external
+ * commands are executed in subprocesses. */
+static int do_pipeline(token_t *token, int ntokens, bool bg) {
+#ifdef DEBUG 0
+  printf("SHELL: do_pipeline\n");
+#endif
+  pid_t pid, pgid = 0;
+  int job = -1;
+  int exitcode = 0;
+
+  int input = -1, output = -1, next_input = -1;
+  mkpipe(&next_input, &output);
+
+  sigset_t mask;
+  Sigprocmask(SIG_BLOCK, &sigchld_mask, &mask);
+
+  /* TODO: Start pipeline subprocesses, create a job and monitor it.
+   * Remember to close unused pipe ends! */
+
+
+
+  Sigprocmask(SIG_SETMASK, &mask, NULL);
+  return exitcode;
+}
+
+static bool is_pipeline(token_t *token, int ntokens) {
+#ifdef DEBUG
+  printf("SHELL: is_pipeline (implemented)\n");
+#endif
+  for (int i = 0; i < ntokens; i++)
+    if (token[i] == T_PIPE)
+      return true;
+  return false;
+}
+
+static void eval(char *cmdline) {
+  bool bg = false;
+  int ntokens;
+#ifdef DEBUG
+  printf("SHELL: eval (implemented)\n");
+#endif
+  token_t *token = tokenize(cmdline, &ntokens);
+
+  if (ntokens > 0 && token[ntokens - 1] == T_BGJOB) {
+    token[--ntokens] = NULL;
+    bg = true;
+  }
+
+  if (ntokens > 0) {
+    if (is_pipeline(token, ntokens)) {
+      do_pipeline(token, ntokens, bg);
+    } else {
+      do_job(token, ntokens, bg);
+    }
+  }
+
+  free(token);
+}
+
+int main(int argc, char *argv[]) {
+  rl_initialize();
+
+  sigemptyset(&sigchld_mask);
+  sigaddset(&sigchld_mask, SIGCHLD);
+
+  initjobs();
+
+  Signal(SIGINT, sigint_handler);
+  Signal(SIGTSTP, SIG_IGN);
+  Signal(SIGTTIN, SIG_IGN);
+  Signal(SIGTTOU, SIG_IGN);
+
+  char *line;
+  while (true) {
+    if (!sigsetjmp(loop_env, 1)) {
+      line = readline("# ");
+    } else {
+      msg("\n");
+      continue;
+    }
+
+    if (line == NULL)
+      break;
+
+    if (strlen(line)) {
+      add_history(line);
+      eval(line);
+    }
+    free(line);
+    watchjobs(FINISHED);
+  }
+
+  msg("\n");
+  shutdownjobs();
+
+  return 0;
+}
