0fd5b2d936483b51f6d86aa5343b556937170a09
Barbara Kaczorowska <bkjg252@gmail.com>
2019-12-29 13:56:12 +0100

    Modified libraries files

--- libcsapp/gai_error.c
+++ libcsapp/gai_error.c
@@ -1,11 +1,11 @@
 #include <stdarg.h>
 #include "csapp.h"
 
-void posix_error(int code, const char *fmt, ...) {
+noreturn void gai_error(int code, const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   vfprintf(stderr, fmt, ap);
-  fprintf(stderr, ": %s\n", strerror(code));
+  fprintf(stderr, ": %s\n", gai_strerror(code));
   va_end(ap);
   exit(EXIT_FAILURE);
 }
--- libcsapp/posix_error.c
+++ libcsapp/posix_error.c
@@ -1,11 +1,11 @@
 #include <stdarg.h>
 #include "csapp.h"
 
-void posix_error(int code, const char *fmt, ...) {
+void unix_error(const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   vfprintf(stderr, fmt, ap);
-  fprintf(stderr, ": %s\n", strerror(code));
+  fprintf(stderr, ": %s\n", strerror(errno));
   va_end(ap);
   exit(EXIT_FAILURE);
 }
